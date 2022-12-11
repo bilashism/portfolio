05608760bb0fd5cf85e7e8636bc3dc1c0a1c4855
Md. Taifuzzaman Bilash <bilashism@gmail.com>
2022-12-11 23:44:37 +0600

    revamped footer

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -1,4 +1,4 @@
-export const APP_NAME = `MT Bilash`;
+export const APP_NAME = `Taifuzzaman Bilash`;
 export const APP_SERVER = `${
   import.meta.env?.VITE_APP_SERVER || `http://localhost:5000`
 }`;
